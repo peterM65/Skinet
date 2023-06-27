b5bf9ffa8bbcc45841a2d3c322cdba286f57df84
peter mamdouh <33961635+peterM65@users.noreply.github.com>
2023-06-27 23:48:25 +0300

    Client-Basket

--- client/src/app/shared/models/basket.ts
+++ client/src/app/shared/models/basket.ts
@@ -1,20 +1,27 @@
+import * as cuid from 'cuid';
+
 export interface BasketItem {
-  id:         number;
-  name:       null;
-  price:      number;
-  quantity:   number;
-  pictureUrl: string;
-  brand:      string;
-  type:       string;
+    id: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    pictureUrl: string;
+    brand: string;
+    type: string;
 }
 
 export interface Basket {
-  id:    string;
-  items: BasketItem[];
+    id: string;
+    items: BasketItem[];
 }
 
-export class Basket implements Basket{
-  id = 'sdsdsdsdsdsd';
-  items: BasketItem[] = [];
+export class Basket implements Basket {
+    id = cuid();
+    items: BasketItem[] = [];
 }
 
+export interface BasketTotals {
+    shipping: number;
+    subtotal: number;
+    total: number;
+}
