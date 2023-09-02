72b318b495eaa59b1fc984c8991834f3624e45c5
peter mamdouh <33961635+peterM65@users.noreply.github.com>
2023-09-02 03:36:49 +0300

    End Project

---
+++ Core/Interfaces/IResponseCacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Interfaces
+{
+    public interface IResponseCacheService
+    {
+        Task CacheResponseAsync(string cachKey, object response, TimeSpan timeToLive);
+        Task<string> GetCacheResponseAsync(string cachKey);
+    }
+}
